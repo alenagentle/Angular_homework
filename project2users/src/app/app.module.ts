d15cdc0917be087541dc316a68f2f699d8b25458
Kiseleva Elena <kiseleva_elena92@mail.ru>
2018-10-31 21:11:14 +0400

    pipes

--- project2users/src/app/app.module.ts
+++ project2users/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { ListComponent } from './list/list.component';
 import { ListItemComponent } from './list-item/list-item.component';
+import { EndPipe } from './list/end.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     ListComponent,
-    ListItemComponent
+    ListItemComponent,
+    EndPipe
   ],
   imports: [
     BrowserModule,
